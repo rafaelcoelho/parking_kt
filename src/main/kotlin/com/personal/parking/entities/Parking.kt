0d97884a664c94cf638819f329072c45d4268012
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-07-27 23:32:57 -0300

    Control the parking capacity

--- src/main/kotlin/com/personal/parking/entities/Parking.kt
+++ src/main/kotlin/com/personal/parking/entities/Parking.kt
@@ -3,7 +3,7 @@ package com.personal.parking.entities
 import java.util.Calendar
 
 class Parking(
-    val capacity: Int,
+    var capacity: Int,
     val openFrom: Calendar,
     val closedFrom: Calendar
 )
@@ -15,21 +15,22 @@ class Parking(
         return currentTime.time > openFrom.time && currentTime.time < closedFrom.time
     }
 
-
     fun checking(car: Car, checkingDate: Calendar): Ticket {
         if (parkedCars.containsKey(car.plate) || !isOpen(checkingDate)) {
             throw RuntimeException("The car is with plate ${car.plate} is already parked")
-        } else if (capacity == parkedCars.size) {
+        } else if (capacity == 0) {
             throw RuntimeException("The parking is full")
         }
 
         val ticket = Ticket(checkingDate, car)
         parkedCars[car.plate] = ticket
+        capacity--
         return ticket
     }
 
     fun isCarParked(car: Car) = parkedCars.containsKey(car.plate)
     fun checkout(paidBill: Ticket.TicketClosed) {
         parkedCars.remove(paidBill.ticket.car.plate)
+        capacity++
     }
 }
