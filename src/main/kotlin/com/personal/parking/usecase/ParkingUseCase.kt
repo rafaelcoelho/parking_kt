723652fa15631759ebbf510608d853f173a0ea99
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-07-27 23:34:31 -0300

    Add CRUD repository adapter wrapped out with Spring

--- src/main/kotlin/com/personal/parking/usecase/ParkingUseCase.kt
+++ src/main/kotlin/com/personal/parking/usecase/ParkingUseCase.kt
@@ -2,22 +2,24 @@ package com.personal.parking.usecase
 
 import com.personal.parking.entities.Car
 import com.personal.parking.entities.Ticket
-import com.personal.parking.repository.ParkingRepository
+import com.personal.parking.usecase.ports.ParkingRepository
+import org.springframework.stereotype.Component
 import java.util.*
 import kotlin.NoSuchElementException
 
+@Component
 class ParkingUseCase(private val repository: ParkingRepository) {
 
     fun park(parkingId: String, carPlate: String, date: Calendar): ParkTicket {
         val parking = repository.getById(parkingId) ?: throw NoSuchElementException(parkingId)
-        return parking.checking(Car(carPlate), date)
-            .toParkTicket()
+        val ticket = parking.checking(Car(carPlate,), date)
+        repository.addCar(parkingId, parking.capacity, ticket)
+        return ticket.toParkTicket()
     }
 
     fun unpark() {}
 
-    data class ParkTicket(val checkin: Calendar, val plate: String) {
-    }
+    data class ParkTicket(val checkin: Calendar, val plate: String)
 
 }
 
