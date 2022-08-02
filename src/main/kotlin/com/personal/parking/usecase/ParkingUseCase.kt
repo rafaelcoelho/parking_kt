df7d424904c64aea72cf9800ab94958477818290
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-08-01 22:00:30 -0300

    Add unpark operation upon use case

--- src/main/kotlin/com/personal/parking/usecase/ParkingUseCase.kt
+++ src/main/kotlin/com/personal/parking/usecase/ParkingUseCase.kt
@@ -11,13 +11,31 @@ import kotlin.NoSuchElementException
 class ParkingUseCase(private val repository: ParkingRepository) {
 
     fun park(parkingId: String, carPlate: String, date: Calendar): ParkTicket {
-        val parking = repository.getById(parkingId) ?: throw NoSuchElementException(parkingId)
-        val ticket = parking.checking(Car(carPlate,), date)
+        val parking = getParkingFrom(parkingId)
+        val ticket = parking.checking(Car(carPlate), date)
         repository.addCar(parkingId, parking.capacity, ticket)
         return ticket.toParkTicket()
     }
 
-    fun unpark() {}
+    fun unpark(closedTicket: Ticket.TicketClosed, tenant: String): Car {
+        val parking = getParkingFrom(tenant)
+
+        val car = closedTicket.ticket.car
+
+        if (!parking.isCarParked(car)) throw NoSuchElementException("The car with plate ${car.plate} is not parked")
+
+        return car.also { parking.checkout(closedTicket) }
+    }
+
+    private fun getParkingFrom(parking: String) = repository.getById(parking) ?:
+        throw NoSuchElementException("The parking $parking not found")
+
+    fun closeTicketFor(plate: String, parking: String, closeAt: Calendar): Ticket.TicketOpen? {
+        val ticket = repository.getById(parking)
+            ?.run { this.parkedCars.getOrDefault(plate, null) }
+
+        return ticket?.close(closeAt)
+    }
 
     data class ParkTicket(val checkin: Calendar, val plate: String)
 
